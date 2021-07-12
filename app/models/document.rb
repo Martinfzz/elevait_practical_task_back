7782f2966e50e813da5a45e3792728e29a12938c
martinfzz <forget.l.martin@gmail.com>
2021-07-12 15:32:19 +0200

    Remove date validation in document model + Create seed

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,10 +3,4 @@ class Document < ApplicationRecord
   validates :title, presence: true, length: {minimum: 1, maximum: 50}
   validates :author, presence: true, length: {minimum: 1, maximum: 50}
   validates :dateCreated, presence: true
-
-  validate :dateCreated_is_valid_datetime
-
-  def happened_at_is_valid_datetime
-    errors.add(:dateCreated, 'must be a valid datetime') if ((DateTime.parse(dateCreated) rescue ArgumentError) == ArgumentError)
-  end
 end
