c06591877aaa2e46ebfeb3cda926bf51feddf4ae
martinfzz <forget.l.martin@gmail.com>
2021-07-12 16:53:28 +0200

    Restrictions on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :pages
-  resources :documents
+  resources :pages, only: [:index, :show, :create]
+  resources :documents, except: [:nex, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
