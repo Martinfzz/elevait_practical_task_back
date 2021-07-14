e8234899a8627be0dc88c44694d0f0dc54a2e00d
martinfzz <forget.l.martin@gmail.com>
2021-07-14 17:58:33 +0200

    General cleaning

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   resources :pages, only: [:index, :show, :create]
-  resources :documents, except: [:nex, :edit]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :documents, only: [:index, :show, :create]
 end
